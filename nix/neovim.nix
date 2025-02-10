cfd0c8065140647b519743b23fe7add8441126a2
46768 <46768@bodin.ac.th>
2025-02-10 14:26:03 +0700

    Generation 47 Mon Feb 10 02:26:03 PM +07 2025

--- nix/neovim.nix
+++ nix/neovim.nix
@@ -1,13 +1,6 @@
 { config, pkgs, ... }:
 
 {
-	xdg.configFile = {
-		"nvim" = {
-			source = "${config.home.homeDirectory}/dotfiles/nvim/nvim";
-			recursive = true;
-			target = "nvim";
-		};
-	};
 	programs.neovim = {
 		enable = true;
 		viAlias = true;
