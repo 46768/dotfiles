c462c0c8cb65ccf65b28c44cf164cd5d603a1224
46768 <46768@bodin.ac.th>
2025-02-13 13:36:40 +0700

    Generation 55 Thu Feb 13 01:36:40 PM +07 2025

--- nix/hyprland/mako.nix
+++ nix/hyprland/mako.nix
@@ -3,5 +3,13 @@
 {
 	home.packages = [
 		pkgs.mako
+		pkgs.libnotify
 	];
+
+	xdg.configFile = {
+		"mako/config" = {
+			source = "${config.xdg.configHome}/home-manager/hyprland/mako/config";
+			target = "mako/config";
+		};
+	};
 }
