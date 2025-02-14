93a162d7eaf408c81b3bb399bf92c0722554b1f7
46768 <46768@bodin.ac.th>
2025-02-14 07:03:35 +0700

    System sync gen 24 Fri Feb 14 07:03:35 AM +07 2025

--- nixos/system.nix
+++ nixos/system.nix
@@ -95,6 +95,7 @@ services.xserver.xkb.options = "eurosign:e,caps:escape,grp:alt_space_toggle";
 	programs.virt-manager.enable = true;
 
 	environment.systemPackages = with pkgs; [
+			linuxKernel.packages.linux_6_6.broadcom_sta
 			git
 
 # Util
