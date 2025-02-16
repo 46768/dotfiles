07744da4a1f52d83fff5d9ce74a66a87593fa18d
46768 <46768@bodin.ac.th>
2025-02-16 15:56:34 +0700

    Generation 10 Sun Feb 16 03:56:33 PM +07 2025

--- nix/hyprland/hyprland.nix
+++ nix/hyprland/hyprland.nix
@@ -193,7 +193,7 @@
 				"$mainMod, mouse:273, resizewindow"
 			];
 
-			"bindre" = [
+			"bindr" = [
 				",XF86AudioRaiseVolume, exec, wpctl set-volume -l $audioLimit @DEFAULT_AUDIO_SINK@ $audioStepI"
 				",XF86AudioLowerVolume, exec, wpctl set-volume -l $audioLimit @DEFAULT_AUDIO_SINK@ $audioStepD"
 				",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
