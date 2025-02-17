de7226856defb5fd882a2da5a2201202a1cb31fa
46768 <46768@bodin.ac.th>
2025-02-17 20:04:50 +0700

    Generation 23 Mon Feb 17 08:04:50 PM +07 2025

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -15,5 +15,14 @@
 				./users.nix
 			];
 		};
+
+		nixosConfigurations.live = nixpkgs.lib.nixosSystem {
+			system = "x86_64-linux";
+			modules = [
+				(nixpkgs + "/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix")
+				./system.nix
+				./users.nix
+			];
+		};
 	};
 }
