ed020cc57977ddbe2f8cfddc7815684844580812
46768 <46768@bodin.ac.th>
2025-03-27 01:18:47 +0700

    Generation 42 Thu Mar 27 01:18:47 AM +07 2025

---
+++ nix/yt-dlp.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, ... }:
+
+{
+	programs.yt-dlp = {
+		enable = true;
+		settings = {
+			downloader = "curl";
+			progress = true;
+			output = "~/yt-download/$(title)s.%(ext)s";
+		};
+	};
+}
